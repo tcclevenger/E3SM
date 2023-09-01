f3eb9b3666af3f70dcc29dacad897a275e5111d9
tcclevenger <tccleve@sandia.gov>
2023-09-01 15:41:16 -0600

    Add my workstations to CIME machines

---
+++ components/eamxx/cmake/machine-files/sandia-workstation.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
+common_setup()
+
+set(CMAKE_Fortran_FLAGS "-fallow-argument-mismatch"  CACHE STRING "" FORCE)
+
+set(HOMME_QUAD_PREC FALSE CACHE BOOL "" FORCE)
+
