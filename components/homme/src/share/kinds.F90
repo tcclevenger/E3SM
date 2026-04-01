28ca185073f21a081a6db7ddb701b7a1b68ac54a
Conrad Clevenger <tccleve@sandia.gov>
2026-04-01 16:19:16 -0400

    WIP

--- components/homme/src/share/kinds.F90
+++ components/homme/src/share/kinds.F90
@@ -19,8 +19,6 @@ module kinds
 implicit none
 private
 !
-!  most floating point variables should be of type real_kind (single or double,
-!  controlled by HOMME_DOUBLE_PRECISION)
 !  For higher precision, we also have quad_kind = real*16, but this
 !  is only supported on IBM systems
 !
@@ -29,22 +27,14 @@ private
   int_kind     = SHR_KIND_I4,            &
   log_kind     = kind(.true.),           &
   long_kind    = SHR_KIND_I8,            &
-#if HOMME_DOUBLE_PRECISION
-  real_kind    = SHR_KIND_R8
-#else
   real_kind    = SHR_KIND_R4
-#endif
 #else
   ! STANDALONE HOMME
   integer (kind=4), public, parameter::  &
   int_kind     = 4,                      &
   long_kind    = 8,                      &
   log_kind     = 4,                      &
-#if HOMME_DOUBLE_PRECISION
   real_kind    = 8
-#else
-  real_kind    = 4
-#endif
 #endif
 
 ! EAM uses iulog from cam_logfile, SCREAM/Homme will declare iulog in this file
